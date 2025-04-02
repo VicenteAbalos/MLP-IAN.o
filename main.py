cec47c048dcdb52c04a4d6743dfe5f4683c0f8f4
MaFeres <maferes@miuandes.cl>
2025-04-02 14:42:26 -0300

    import settup.

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
